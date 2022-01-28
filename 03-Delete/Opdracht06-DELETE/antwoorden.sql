f1925d883d4eee4673accdeeb73547c027f059db
Kris <krdelver@gmail.com>
2022-01-28 16:57:20 +0100

    06

--- 03-Delete/Opdracht06-DELETE/antwoorden.sql
+++ 03-Delete/Opdracht06-DELETE/antwoorden.sql
@@ -3,7 +3,7 @@ DELETE FROM player WHERE name = "David Silva";
 -- Opdracht 2 
 DELETE FROM players WHERE club = "Willem II";
 -- Opdracht 3
-DELETE FROM player WHERE nationality = "belgische" AND club = "FC Barcelona";
+DELETE FROM players WHERE nationality = "Belgium" AND club = "FC Barcelona";
 -- Opdracht 4
 DELETE FROM player WHERE age >= 38;
 -- Opdracht 5
